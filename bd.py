8d138b86cfc54cc973f9a77844556e4715db5a2a
vladorthebest <vladorthebest@gmail.com>
2022-03-08 21:32:58 +0100

    add bd, keayboard_bot, bot v2.0

--- bd.py
+++ bd.py
@@ -1,23 +1,93 @@
 import sqlite3
-import parse
+import requests
+from bs4 import BeautifulSoup as BS
+from datetime import datetime
+import json
 
 
-con = sqlite3.connect('data_base.db')
-cursor = con.cursor()
+class BD():
+    con = sqlite3.connect('data_base.db', check_same_thread=False)
+    cursor = con.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS menu
-                    (title text, model text, price text, path text)''')
-
-
-def set_bd(title, model, price, path):
-    
-    query = '''INSERT INTO menu VALUES 
-                    (?, ?, ?, ?)'''
-    
-    cursor.execute(query, (title, model, price, path))
-    con.commit()
+    def __init__(self):    
+        BD.cursor.execute('''CREATE TABLE IF NOT EXISTS menu
+                            (day int, title text, model text, price text, path text)''')
         
-def output_bd():
-    for row in cursor.execute(''' SELECT * FROM menu'''):
-            print(row)
+        self.parse()
+
+
+    def _set_bd(self, day, title, model, price, path):
         
+        query = '''INSERT INTO menu VALUES 
+                        (?, ?, ?, ?, ?)'''
+        print(title)
+        BD.cursor.execute(query, (day, title, model, price, path))
+        BD.con.commit()
+            
+
+    def get_bd(self, types):
+
+        with open('date.json', 'r') as date_json:
+            day = json.load(date_json)
+
+        if day == int(self._day()):
+            menu = []
+            for type in types:
+                for item in BD.cursor.execute(''' SELECT * FROM menu WHERE title = ? ''', (type,)):
+                    menu.append( item )
+            return menu
+
+        else:
+            self.parse()
+            self.get_bd()
+        
+
+
+    def _day(self):
+        today = datetime.today()
+        return today.isoweekday()
+    
+    
+    def _save_img(self, item):
+        img_url=item.find('img', class_="img-responsive center-block")['src']
+        img = requests.get(img_url)
+        img_path = 'img\ ' + item.find('h4').get_text(strip = True) + '.jpg'
+        img_file = open(img_path, 'wb')
+        img_file.write(img.content)
+        img_file.close()
+        return 'img\ ' + item.find('h4').get_text(strip = True) + '.jpg'
+
+    def _clear_bd(self):
+        sql_delete_query = """DELETE from menu"""
+        BD.cursor.execute(sql_delete_query)
+        BD.con.commit()
+
+    def parse(self):
+        self._clear_bd()
+        r = requests.get("http://eatandmeet.sk/")
+        html = BS(r.content, 'html.parser')
+        today = str(self._day())
+        id_today = 'day-' + today
+        blok = html.find(id = id_today)
+        items = blok.find_all('div', class_='col-lg-6 col-md-6 col-sm-6')
+
+        for item in items:
+            path_img = self._save_img(item) 
+            title = item.find('h4').get_text(strip = True)
+            if ' ' in title:
+                title = title[:title.index(' ')]
+            menu = item.find('p',  class_="desc").get_text(strip = True)
+            price = item.find('span', class_="price").get_text(strip = True)
+            
+            self._set_bd(today, title, menu, price, path_img)   
+
+        with open('date.json', 'w') as date_json:
+            json.dump(int(today), date_json)
+
+
+
+
+if __name__ == '__main__':
+    bd = BD()
+    bd.parse()
+    bd.get_bd()
--- bot.py
+++ bot.py
@@ -1,36 +1,55 @@
-import parse
+from bd import BD 
 import telebot
 import config
-
 
 bot = telebot.TeleBot(config.TOKEN)
 
 
+main_kayboard = telebot.types.ReplyKeyboardMarkup(True, False)
+main_kayboard.row('Супы')
+main_kayboard.row('Порции')
+main_kayboard.row('Остальное')
+
+
+
 def build_item(item):
     message_menu = ''
-    title = item['title']
-    menu = item['menu']
-    price = item['price']
-    
-    message_menu +=  f'📄  {title} \n\n'
-    message_menu +=  f'🍽  {menu} \n'
-    message_menu +=  f'💵💵💵  {price} \n'
+    for day, title, name, price in item:
+        message_menu +=  f'📄  {title} \n\n'
+        message_menu +=  f'🍽  {name} \n'
+        message_menu +=  f'💵💵💵  {price} \n'
     return message_menu
 
 
-@bot.message_handler(commands=['menu'])
+@bot.message_handler(commands="start")
+def cmd_start(message):
+    bot.send_message(message.chat.id, "Что вам показать?", reply_markup=main_kayboard)
+
+
+@bot.message_handler(content_types=['text'])
 def menu(message):
+    if message.text == 'Порции': 
+        menu = bd.get_bd(("MENU", ))
 
-    parse.parse()
+    elif message.text == 'Супы': 
+        menu = bd.get_bd(("Polievka", ))
 
-    for item in count:
-        
-        photo = open(item['path'], 'rb')
+    elif message.text == 'Остальное': 
+        menu = bd.get_bd(('SLADKÉ', 'MÚČNE', 'VEGETARIÁNSKE', 'ŠALÁT'))
+    
+    else:
+        bot.send_message(message.chat.id,'Нет такой категории')
+
+    for item in menu:     
+        path = item[-1]
+        photo = open(path, 'rb')
         bot.send_photo(message.chat.id, photo)
 
-        message_menu = build_item(item)      
+        message_menu = build_item([item[:-1]])      
         bot.send_message(message.chat.id, message_menu)
-   
+    
+
 
 if __name__ == '__main__':
+    bd = BD()
     bot.polling(none_stop=True)
